3d9f7e7c818f4bb8fa2ea05c7416b57be48351e8
phamthang2003 <116180596+phamthang2003@users.noreply.github.com>
2022-10-20 20:20:50 +0700

    Add files via upload
    anhme

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'mẹ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng mẹ iu';
+const contentCard = 'con chúc mẹ 20/10 thật nhiều ý nghĩa, chúc mẹ luôn luôn mạnh khỏe, yêu đời, xinh đẹp và thành công trong công việc nhé ạ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anhmehien.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
